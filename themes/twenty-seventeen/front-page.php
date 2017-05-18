e7902121938ff9f0d9f58ef910b9e1c6b21ea4a2
Vincent Ragosta <vragosta@storycorps.org>
2017-05-18 10:20:10 -0400

    continued work on introduction, started working on recent works section

--- themes/twenty-seventeen/front-page.php
+++ themes/twenty-seventeen/front-page.php
@@ -4,9 +4,16 @@ get_header();
 
 ?>
 
-<section>
+<section class="introduction">
 	<div>
-		<h2>Welcome to my website!</h2>
-		<h2>Hello, i'm Vincent Ragosta</h2>
+		<h3>Welcome to my website!</h3>
+		<h2>Hello, i’m Vincent Ragosta...</h2>
+		<h4>...and I believe in producing high-quality work that fuels the engagement between brands and their audience. My passion is design & technology.</h4>
+	</div>
+</section>
+
+<section class="work">
+	<div class="section-ico">
+
 	</div>
 </section>
